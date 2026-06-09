80c7f38cf0591fc1344efcf5e1fd318cbcb1efc7
knothhe <knothhe@gmail.com>
2026-06-09 18:34:59 +0800

    refactor: add timing instrumentation to Bigint benchmark

--- gmp-rs/src/main.rs
+++ gmp-rs/src/main.rs
@@ -1,12 +1,18 @@
 use gmp_rs::Bigint;
+use std::time::Instant;
 
 fn main() {
     let mut a = Bigint::new(0);
     let mut b = Bigint::new(1);
-    for _ in 0..10000 {
-        println!("{a}");
 
+    let start_now = Instant::now();
+    for _ in 0..3000_00 {
         a.add(&b);
         std::mem::swap(&mut a, &mut b);
     }
+    let end_now = Instant::now();
+    // new_now.duration_since(now)
+    let elapsed_time_sec = end_now.duration_since(start_now).as_secs_f64();
+    // print!("{a}\n");
+    print!("elapsed time with bigint: {elapsed_time_sec}");
 }
